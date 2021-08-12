5bba412c19d8c4987687e36c681e57b156fba532
Hyliao711 <harryj98038@gmail.com>
2021-08-12 17:13:10 +0800

    Update 2021-08-12T09:13:08.655Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,18 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var menuBtn = document.querySelector('.menu-btn');
+var dropDown = document.querySelector('.dropdown');
+menuBtn.addEventListener('click', function () {
+  // if(!menuOpen) {
+  //   menuBtn.classList.add('open');
+  //   dropDown.classList.add('open');
+  //   menuOpen = true;
+  // } else {
+  //   menuBtn.classList.remove('open');
+  //   menuOpen = false;
+  // }
+  menuBtn.classList.toggle('open');
+  dropDown.classList.toggle('open');
+});
 //# sourceMappingURL=all.js.map
